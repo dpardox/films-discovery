63bec7ce0fceb1741009b464e9d9f55999010000
Steven Pardo <dspardo6@gmail.com>
2021-12-06 22:38:44 -0500

    Header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WebLayoutComponent } from './layouts/web-layout/web-layout.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WebLayoutComponent
+    WebLayoutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    HeaderComponent
+  ]
 })
 export class AppModule { }
